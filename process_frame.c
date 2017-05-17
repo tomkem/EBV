a1c178567d10b1d2aa8a2520c5e3a34be28748f2
oscar <oscar@oscar.(none)>
2017-05-17 21:27:36 +0200

    testat02

--- process_frame.c
+++ process_frame.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 
 #define IMG_SIZE NUM_COLORS*OSC_CAM_MAX_IMAGE_WIDTH*OSC_CAM_MAX_IMAGE_HEIGHT
+#define YCBCR  		// YCBCR or RGB
 
 const int nc = OSC_CAM_MAX_IMAGE_WIDTH;
 const int nr = OSC_CAM_MAX_IMAGE_HEIGHT;
@@ -25,6 +26,19 @@ const int Border = 2;
 /* minimum size of objects (sum of all pixels) */
 const int MinArea = 500;
 
+/* Number of different Object colors*/
+const int NumFgrCol = 2;
+
+/* Colors of objects*/
+#ifdef YCBCR
+uint8 FrgCol[2][3] = {{65,120,160},{60,145,120}}; // blau, rot
+#else if RGB
+uint8 FrgCol[2][3] = {{50,43,30},{20,37,105}}; // blau, rot
+#endif
+
+/* maximum Objects*/
+int objectColor[200][NUM_COLORS+1];			// the last item for the found color
+
 /* size of centroid marker */
 const int SizeCross = 10;
 
@@ -36,7 +50,9 @@ void Erode_3x3(int InIndex, int OutIndex);
 void Dilate_3x3(int InIndex, int OutIndex);
 void DetectRegions();
 void DrawBoundingBoxes();
-
+void ChangeDetection();
+void RGBtoACbCr();
+void getObjectColor();
 
 void ResetProcess()
 {
@@ -54,14 +70,16 @@ void ProcessFrame() {
 		ManualThreshold = false;
 	} else {
 		unsigned char Threshold = OtsuThreshold(SENSORIMG);
-
-		Binarize(Threshold);
-
-		Erode_3x3(THRESHOLD, INDEX0);
-		Dilate_3x3(INDEX0, THRESHOLD);
+#ifdef YCBCR
+		RGBtoACbCr();
+#endif
+		ChangeDetection();
+		//Erode_3x3(THRESHOLD, INDEX0);
+		//Dilate_3x3(INDEX0, THRESHOLD);
 
 		DetectRegions();
-
+		getObjectColor();
+		//Binarize(Threshold);
 		DrawBoundingBoxes();
 
 		if(ManualThreshold) {
@@ -178,7 +196,7 @@ void DetectRegions() {
 
 	//set pixel value to 1 in INDEX0 because the image MUST be binary (i.e. values of 0 and 1)
 	for(i = 0; i < IMG_SIZE; i++) {
-		data.u8TempImage[INDEX0][i] = data.u8TempImage[THRESHOLD][i] ? 1 : 0;
+		data.u8TempImage[INDEX0][i] = data.u8TempImage[INDEX1][i] ? 1 : 0;
 	}
 
 	//wrap image INDEX0 in picture struct
@@ -198,15 +216,108 @@ void DrawBoundingBoxes() {
 	for(o = 0; o < ImgRegions.noOfObjects; o++) {
 		if(ImgRegions.objects[o].area > MinArea) {
 			DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
-							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
+							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, (uint8)objectColor[o][3]);
 
 			DrawLine(ImgRegions.objects[o].centroidX-SizeCross, ImgRegions.objects[o].centroidY,
-					 ImgRegions.objects[o].centroidX+SizeCross, ImgRegions.objects[o].centroidY, RED);
+					 ImgRegions.objects[o].centroidX+SizeCross, ImgRegions.objects[o].centroidY, (uint8)objectColor[o][3]);
 			DrawLine(ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY-SizeCross,
-								 ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY+SizeCross, RED);
+								 ImgRegions.objects[o].centroidX, ImgRegions.objects[o].centroidY+SizeCross, (uint8)objectColor[o][3]);
 
 		}
 	}
 }
 
+void ChangeDetection() {
+	int r, c, frg, p;
 
+	memset(data.u8TempImage[INDEX0], 0, IMG_SIZE);
+	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
+	memset(data.u8TempImage[BACKGROUND], 0, IMG_SIZE);
+
+	//loop over the rows
+	for(r = 0; r < nr*nc; r += nc) {
+		//loop over the columns
+		for(c = 0; c < nc; c++) {
+			//loop over the different Frg colors and find smallest difference
+			int MinDif = 1 << 30;
+			int MinInd = 0;
+
+			for(frg = 0; frg < NumFgrCol; frg++) {
+				int Dif = 0;
+
+				//loop over the color planes (r, g, b) and sum up the difference
+				for(p = 1; p < NUM_COLORS; p++) {
+					Dif += abs((int) data.u8TempImage[THRESHOLD][(r+c)*NUM_COLORS+p]- (int) FrgCol[frg][p]);
+				}
+
+				if(Dif < MinDif) {
+					MinDif = Dif;
+					MinInd = frg;
+				}
+			}
+			//if the difference is smaller than threshold value
+			if(MinDif < data.ipc.state.nThreshold) {
+				//set pixel value to 255 in THRESHOLD image for further processing
+				//(we use only the first third of the image buffer)
+				data.u8TempImage[INDEX1][(r+c)] = 255;
+				//set pixel value to Frg color in BACKGROUND image for visualization
+				for(p = 0; p < NUM_COLORS; p++) {
+					data.u8TempImage[BACKGROUND][(r+c)*NUM_COLORS+p] = FrgCol[MinInd][p];
+				}
+			}
+		}
+	}
+}
+
+void RGBtoACbCr() {
+	int r, c;
+	for(r = 0; r < nr*nc; r += nc) {
+		//loop over the columns
+		for(c = 0; c < nc; c++) {
+			//get rgb values (order is actually bgr!)
+			float B_ = data.u8TempImage[SENSORIMG][(r+c)*NUM_COLORS+0];
+			float G_ = data.u8TempImage[SENSORIMG][(r+c)*NUM_COLORS+1];
+			float R_ = data.u8TempImage[SENSORIMG][(r+c)*NUM_COLORS+2];
+
+			uint8 Y_ = (uint8) ( 0 + 0.299*R_ + 0.587*G_ + 0.114*B_);
+			uint8 Cb_ = (uint8) (128 - 0.169*R_ - 0.331*G_ + 0.500*B_);
+			uint8 Cr_ = (uint8) (128 + 0.500*R_ - 0.419*G_ - 0.081*B_);
+
+			//we write result to THRESHOLD
+			data.u8TempImage[THRESHOLD] [(r+c)*NUM_COLORS+0] = Y_;
+			data.u8TempImage[THRESHOLD] [(r+c)*NUM_COLORS+1] = Cb_;
+			data.u8TempImage[THRESHOLD] [(r+c)*NUM_COLORS+2] = Cr_;
+		}
+	}
+}
+
+void getObjectColor(){
+	int o, c, p;
+
+	//loop over objects
+	for(o = 0; o < ImgRegions.noOfObjects; o++) {
+		for(p = 0; p < NUM_COLORS; p++) {
+			objectColor[o][p] = 0;
+		}
+		//get pointer to root run of current object
+		struct OSC_VIS_REGIONS_RUN* currentRun = ImgRegions.objects[o].root;
+		//loop over runs of current object
+		do {
+			//loop over pixel of current run
+			for(c = currentRun->startColumn; c <= currentRun->endColumn; c++) {
+				int r = currentRun->row;
+				//loop over color planes of pixel
+				for(p = 0; p < NUM_COLORS; p++) {
+					objectColor[o][p] += data.u8TempImage[SENSORIMG][(r*nc+c)*NUM_COLORS+p];
+				}
+			}
+			currentRun = currentRun->next;
+		} while(currentRun != NULL);
+
+		if(objectColor[o][0] > objectColor[o][2]){
+			objectColor[o][3] = BLUE;
+		} else {
+			objectColor[o][3] = RED;
+		}
+	}
+}
--- template_ipc.h
+++ template_ipc.h
@@ -13,7 +13,7 @@
 
 /*! @brief set to three to work with color images */
 /* IMPORTANT!!: do a clean after changing this value to ensure rebuild of cgi-script */
-#define NUM_COLORS 1
+#define NUM_COLORS 3
 
 /* The parameter IDs to identify the different requests/responses. */
 enum EnIpcParamIds
